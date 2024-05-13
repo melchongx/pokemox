a986e19592a6c7a934baa3a51265c2a9620c5a47
John Daniel De Castro <decastrojohndaniel@gmail.com>
2024-05-13 20:33:44 +0800

    add sorting functions on pokemon data

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -6,4 +6,35 @@ const padStartId = (id) => {
   return String(id).padStart(4, "0");
 };
 
-export { generateRandomPokemonId, padStartId };
+const sortPokemonData = (dataArr, sortBy) => {
+  const updatedArr = [...dataArr];
+  switch (sortBy) {
+    case "number":
+      updatedArr.sort((a, b) => a.id - b.id);
+      break;
+    case "name":
+      updatedArr.sort((a, b) => {
+        const nameA = a.name.toLowerCase();
+        const nameB = b.name.toLowerCase();
+        if (nameA < nameB) return -1;
+        if (nameA > nameB) return 1;
+        return 0;
+      });
+      break;
+    case "type":
+      updatedArr.sort((a, b) => {
+        const typeA = a.types[0].type.name.toLowerCase();
+        const typeB = b.types[0].type.name.toLowerCase();
+        if (typeA < typeB) return -1;
+        if (typeA > typeB) return 1;
+        return 0;
+      });
+      break;
+    default:
+      throw new Error("Invalid sort option.");
+  }
+
+  return updatedArr;
+};
+
+export { generateRandomPokemonId, padStartId, sortPokemonData };
