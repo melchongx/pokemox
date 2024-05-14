25ff8897bce50f5f3ad5f7cde9185d790e0cf154
melchongx <melfredfonclara@gmail.com>
2024-05-14 21:16:24 +0800

    ballin

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { fetchAllPokemonData } from "./api/api.js";
+import { pokemons } from "./data/teams.jsx";
 
 import RootLayout from "./components/RootLayout";
 import Home from "./pages/Home";
@@ -8,6 +11,17 @@ import PokemonView from "./pages/PokemonView.jsx";
 import CreateTeam from "./pages/CreateTeam.jsx";
 
 const App = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchAllPokemonData();
+      data.sort((a, b) => a.id - b.id);
+
+      pokemons.push(data);
+      console.log(pokemons);
+    };
+
+    fetchData();
+  }, []);
   return (
     <Router>
       <Routes>
